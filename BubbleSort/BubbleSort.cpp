4028f44e198f42affbc8ddc61cb87038616906f5
ZhilPro27 <zhilalkrisna@gmail.com>
2024-03-08 15:44:18 +0700

    Fungsi Bubble Sort

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -21,7 +21,29 @@ void input() {	//Prosedur untuk input.
 	cout << "=====================" << endl;
 
 	for (int i = 0; i < n; i++) { //Looping dengan i dimulai dari 0 hingga n-1
-		cout << "Data ke-" << (i + 1) << ": "; //Output ke layar
-		cin >> arr[i];
+		cout << "Data ke-" << (i + 1) << "= "; //Output ke layar
+		cin >> arr[i];			//Output dari pengguna
 	}
+}
+
+void bubblesortArray() {		//Prosedur untuk mengurutkan array dengan metode bubble sort
+	int pass = 1; //Step 1
+
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) { //Step 2
+			if (arr[j] > arr[j + 1]) {	//Step 3
+				int temp;
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+		pass = pass + 1; //Step 4
+
+		cout << "\nPass" << pass - 1 << "= "; //Number of pass
+		for (int k = 0; k < n; k++) {
+			cout << arr[k] << " "; //Menampilkan data pada number of pass
+		}
+		cout << endl;
+	} while (pass <= n - 1); //Step 5
 }
